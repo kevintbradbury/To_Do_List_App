dba5beb1570c1f360661809dcaab91dfc1949745
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-08-31 10:01:32 -0700

    Add pic files

---
+++ GroceriesPage.swift
@@ -0,0 +1,41 @@
+//
+//  GroceriesPage.swift
+//  To_Do_List
+//
+//  Created by K on 8/30/16.
+//  Copyright © 2016 K. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GroceriesPage: UIViewController {
+    
+    @IBOutlet weak var groceriesTextField: UITextField!
+    
+    @IBOutlet weak var saveListButton: UIButton!
+    
+    @IBOutlet weak var appleButton: UIButton!
+    
+    @IBAction func appleTextToField(_ sender: AnyObject) {
+        
+    }
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
